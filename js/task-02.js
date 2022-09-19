ca3f050853ec9709b7f15b76d6f7196f238e7da1
Andriy-Martsynyak <a.marcynak@ukr.net>
2022-09-19 18:21:10 +0300

    completes task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,8 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+ingredients.forEach(ingredient => {
+  const list = document.getElementById('ingredients');
+  const listItem = document.createElement('li');
+  listItem.textContent = `${ingredient}`;
+  listItem.classList.add('item');
+  list.append(listItem);
+});
